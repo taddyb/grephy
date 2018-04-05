df0bd4374bc84da19ed0a3dc4bd599dcc97d52a6
taddyb <tadd.bindas1@marist.edu>
2018-04-05 13:07:22 -0400

    Added a few more functions to learn the alphabet of the input file, to check if a regular expression is valid, and to learn the alphabet of the input file

--- grepy.py
+++ grepy.py
@@ -1,61 +1,54 @@
 import argparse
+import re
 
+#A main function
 def main():
     args = parseArguments()
+    if(checkRegex(args.regex)):
+        alphabet = learnAlphabet(args.inputFile)
+        print(alphabet)
+    else:
+        error(100)
 
+#A function to parse the input and create a --help message
 def parseArguments():
     parser = argparse.ArgumentParser(description='A program which emulates grep')
     parser = argparse.ArgumentParser(prog='Grepy')
     parser.add_argument('-n', metavar='NFA_FILE', type=argparse.FileType('rw'), help="A file to output an NFA of the regular expression to. Optional")
     parser.add_argument('-d', metavar='DFA_FILE', type=argparse.FileType('rw'), help="A file to output a DFA of the regular expression to. Optional")
-    parser.add_argument('REGEX', metavar='Regex_Expression', type=str, help='A regular expression to search the input file')
-    parser.add_argument('FILE', metavar='Input_File', type=argparse.FileType('r'), help="A file which the Regex_Expression checks for matches")
+    parser.add_argument('regex', metavar='Regex_Expression', type=str, help='A regular expression to search the input file')
+    parser.add_argument('inputFile', metavar='Input_File', type=argparse.FileType('r'), help="A file which the Regex_Expression checks for matches")
 
     args = parser.parse_args()
     return args;
 
+# A function which checks to make sure the regular expression is valid
+def checkRegex(regex):
+    try:
+        re.compile(regex)
+        return True;
+    except re.error:
+        return False;
+
+#A function to learn the alphabet of the input file
+def learnAlphabet(inputFile):
+    alphabet = set()
+    inputContents = inputFile.read()
+    inputFile.close()
+    for character in inputContents:
+        if (ord(character) > 47) and (ord(character) < 123):
+            alphabet.add(character)
+    return alphabet;
+
+
+#A function which takes in error codes and prints out error messages based on what is inputted
+def error(code):
+    if(code == 100):
+        print("*************************\n ERROR: Regular Expression not valid\n*************************")
+
+
 if __name__ == "__main__":
     main()
-
-#Old code
-#
-# import sys
-#
-#
-# nfaFile = open(sys.argv[2],"w")
-# # dfaFile = open(sys.argv[2],"w")
-# inputFile = open(sys.argv[1],"r")
-#
-# inputContents = inputFile.read()
-# inputFile.close()
-#
-# nfaList = []
-# alphabetList = []
-# stateCount = 0
-# nfaList.append([stateCount])
-# stateCount = stateCount + 1
-#
-# for character in inputContents:
-#     if (ord(character) > 47) and (ord(character) < 123):
-#         alphabetList.append(character)
-#         nfaList.append([stateCount])
-#         stateCount = stateCount + 1
-#
-# alphabetSet = set(alphabetList)
-#
-# print "Basic NFA"
-# for position in nfaList:
-#     if(position == nfaList[0]):
-#         print position
-#     else:
-#         print "--"
-#         print position
-#
-# print nfaList
-#
-# print "Alphabet:"
-# print alphabetSet
-
 
 # Next Steps
 # 1) write NFA to file.
