97ec5a8f787e7291b9d2cdb91f7e4341fdf00069
taddyb <tadd.bindas1@marist.edu>
2018-04-05 11:59:10 -0400

    I realized all of my code is trash and I should redo it. I read up on how to make python functions and made a function to take in the arguments using argparse. Credit to G Leaden for showing me argparse !

--- grepy.py
+++ grepy.py
@@ -1,35 +1,50 @@
-import sys
+import argparse
 
+def main():
+    args = parseArguments()
 
-nfaFile = open(sys.argv[2],"w")
-# dfaFile = open(sys.argv[2],"w")
-inputFile = open(sys.argv[1],"r")
+def parseArguments():
+    parser = argparse.ArgumentParser(description='A program which emulates grep')
+    parser = argparse.ArgumentParser(prog='Grepy')
+    parser.add_argument('-n', metavar='NFA_FILE', type=argparse.FileType('rw'), help="A file to output an NFA of the regular expression to. Optional")
+    parser.add_argument('-d', metavar='DFA_FILE', type=argparse.FileType('rw'), help="A file to output a DFA of the regular expression to. Optional")
+    parser.add_argument('REGEX', metavar='Regex_Expression', type=str, help='A regular expression to search the input file')
+    parser.add_argument('FILE', metavar='Input_File', type=argparse.FileType('r'), help="A file which the Regex_Expression checks for matches")
 
-inputContents = inputFile.read()
-inputFile.close()
+    args = parser.parse_args()
+    return args;
 
-nfaList = []
-alphabetList = []
-stateCount = 0
-nfaList.append([stateCount])
-stateCount = stateCount + 1
-
-for character in inputContents:
-    if (ord(character) > 47) and (ord(character) < 123):
-        alphabetList.append(character)
-        nfaList.append([stateCount])
-        stateCount = stateCount + 1
-
-alphabetSet = set(alphabetList)
-
-for position in nfaList:
-    if(position == nfaList[0]):
-        print position
-    else:
-        print "--"
-        print position
-
-print nfaList
+if __name__ == "__main__":
+    main()
+# nfaFile = open(sys.argv[2],"w")
+# # dfaFile = open(sys.argv[2],"w")
+# inputFile = open(sys.argv[1],"r")
+#
+# inputContents = inputFile.read()
+# inputFile.close()
+#
+# nfaList = []
+# alphabetList = []
+# stateCount = 0
+# nfaList.append([stateCount])
+# stateCount = stateCount + 1
+#
+# for character in inputContents:
+#     if (ord(character) > 47) and (ord(character) < 123):
+#         alphabetList.append(character)
+#         nfaList.append([stateCount])
+#         stateCount = stateCount + 1
+#
+# alphabetSet = set(alphabetList)
+#
+# for position in nfaList:
+#     if(position == nfaList[0]):
+#         print position
+#     else:
+#         print "--"
+#         print position
+#
+# print nfaList
 
 # Next Steps
 # 1) write NFA to file.
