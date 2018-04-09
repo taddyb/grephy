38f1f46c89d16b94b0358621cc9191038bfe059d
taddyb <tadd.bindas1@marist.edu>
2018-04-09 13:45:49 -0400

    Added a config file for advanced debugging

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Grephy
 A python script which emulates grep.
 
+To use this you must have ```networkx``` installed. To do so, run  `pip install networkx`
+
 To run `python grephy.py [-n NFA_FILE] [-d DFA_FILE] REGEX FILE`
--- grephy.py
+++ grephy.py
@@ -1,14 +1,68 @@
 import argparse
 import re
+import networkx as nx
+import ConfigParser
+import io
+
+#A class of an NFA
+class NFA(object):
+    # digraph of epsilon transitions
+    graph = ""
+    # regular expression
+    regexp = ""
+    #number of characters in regular expression
+    m = 0
+    def __init__(regexp):
+        self.regexp = regexp
+        self.m = rexexp.len()
+        self.graph = nx.Digraph()
+        ops = Stack()
+        for i in range(0,m):
+            lp = i
+            if(rexexp[i] == '(' or rexexp[i] == '|'):
+                ops.push(i)
+            elif(regex[i] == ')'):
+                ore = ops.pop()
+                if(regexp[ore] == '|'):
+                    lp = ops.pop()
+                    self.graph.add_edge(lp, ore + 1)
+                    self.graph.add_edge(ore, i)
+
+                elif(regexp[ore] == '('):
+                    lp = ore
+                else:
+                    assert False
+
+            # If there is a kleene star, the code maps back to itself
+            if(i < m-1 and regexp[i+1] == '*'):
+                self.graph.add_edge(lp, i+1)
+                self.graph.add_edge(i+1, lp)
+
+            if(regexp[i] == '(' or regexp[i] == '|' or regexp[i] == '*'):
+                self.graph.add_edge(i, i+1)
+
+
 
 #A main function
 def main():
+    config = readConfig("config/config.ini")
     args = parseArguments()
     if(checkRegex(args.regex)):
         alphabet = learnAlphabet(args.inputFile)
-        print(alphabet)
+        if(config.get('DEBUG', 'printAlpha')):
+            print(alphabet)
+        # nfa = NFA(args.regex)
+        # print(nfa.graph.edges())
     else:
         error(100)
+
+# A function to read the ini config FILE
+def readConfig(configPath):
+    with open(configPath) as f:
+        sample_config = f.read()
+        config = ConfigParser.RawConfigParser(allow_no_value=True)
+        config.readfp(io.BytesIO(sample_config))
+    return config;
 
 #A function to parse the input and create a --help message
 def parseArguments():
@@ -40,6 +94,9 @@ def learnAlphabet(inputFile):
             alphabet.add(character)
     return alphabet;
 
+#A function which converts a regex expression to an NFA
+def regexToNFA(regex):
+    return;
 
 #A function which takes in error codes and prints out error messages based on what is inputted
 def error(code):
@@ -49,9 +106,3 @@ def error(code):
 
 if __name__ == "__main__":
     main()
-
-# Next Steps
-# 1) write NFA to file.
-# 2) use alphabet to make a dfa
-# 3) minimize the dfaFile
-# 4) make an accepting function
