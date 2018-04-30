dccc2111682a260fd77a1ada97378e09df867d57
taddyb <tadd.bindas1@marist.edu>
2018-04-30 19:59:28 -0400

    Made a recognize function

--- grephy.py
+++ grephy.py
@@ -21,31 +21,35 @@ class NFA(object):
         self.graph = nx.DiGraph()
         state = 0
         i = 0
+        string = ""
+        info = []
         ops = []
         while(i < self.m):
-            if(self.regexp[i] == '(' or self.regexp[i] == '|'):
-                ops.append(state)
-
-            elif(self.regexp[i] == ')'):
+            if(self.regexp[i] == '('):
+                ops.append(i)
+            if(self.regexp[i] == '|'):
+                info.append(string)
+                string=""
+            if(self.regexp[i] == ')'):
                 ore = ops.pop()
+                if (self.regexp[ore] == '('):
+                    print(len(ops))
+                    info.append(string)
+                    string = ""
+                    if(len(ops) == 0):
+                        self.graph.add_edge(state, state, transition=info)
 
-                if(self.regexp[ore] == '|'):
-                    lp = ops.pop()
-                    self.graph.add_edge(lp, ore + 1, transition=self.regexp[i])
 
-                elif(self.regexp[ore] == '('):
-                    lp = ore
-                else:
-                    assert False
-
-            # If there is a kleene star, the code maps back to itself
             if(i < (self.m-1) and self.regexp[i+1] == '*'):
                 self.graph.add_edge(state, state, transition=self.regexp[i])
 
+            if((self.regexp[i] == '(' or self.regexp[i] == '|' or self.regexp[i] == '*' or self.regexp[i] == ')') == False):
+                if(len(ops) > 0):
+                    string = string + self.regexp[i]
+                else:
+                    self.graph.add_edge(state, state+1, transition=self.regexp[i])
+                    state = state + 1
 
-            elif((self.regexp[i] == '(' or self.regexp[i] == '|' or self.regexp[i] == '*') == False):
-                self.graph.add_edge(state, state+1, transition=self.regexp[i])
-                state = state + 1
             i = i + 1
 
     def recognizes(self, text):
@@ -53,67 +57,42 @@ class NFA(object):
         state = 0
         text.replace('.', '')
         words = text.split()
+        length = self.graph.order()
         for i in range(0,len(words)):
             word = words[i]
-            for j in word:
-                if(len(self.graph.successors(state)) > 1):
-                    successors = self.graph.successors(state)
-                    i = 0
-                    while(i < len(successors) or i != -1):
-                        if(word[j] == self.graph[state][successors[i]]['transtition']):
-                            state = successors[i]
-                            i = -1
-                            if(self.graph.successors(state) == []):
-                                recog.append(word)
-                        else:
-                            i = i + 1
-                elif(word[j] == self.graph[state][state+1]['transtition']):
-                    state = state +1
-                    if(self.graph.successors(state) == []):
-                        recog.append(word)
-                else:
-                    break
+            for char in word:
+                if(state != -1):
+                    listStates = self.graph[state].keys()
+                    if(len(listStates) > 1):
+                        for i in listStates:
+                            if(isinstance(i, list)):
+                                for j in i:
+                                    if(char == self.graph[state][i]['transition']):
+                                        print(i)
+                                        state = i
+                                    if(state == (length -1)):
+                                        recog.append(word)
+                                        state= -1
+                            else:
+                                if(char == self.graph[state][i]['transition']):
+                                    print(i)
+                                    state = i
+                                if(state == (length -1)):
+                                    recog.append(word)
+                                    state= -1
+                    elif(char == self.graph[state][state+1]['transition']):
+                        state = state +1
+                        if(state == (length -1)):
+                            recog.append(word)
+                            state = -1
+            state = 0
         return recog;
-
 
 
 
 ##################################################################################
 # DFA class
 ##################################################################################
-class DFA(object):
-        def __init__(self, regexp):
-            self.regexp = regexp
-            self.m = len(regexp)
-            self.graph = nx.DiGraph()
-            ops = []
-            for i in range(0,self.m):
-                lp = i
-                if(self.regexp[i] == '(' or self.regexp[i] == '|'):
-                    ops.append(i)
-                elif(self.regexp[i] == ')'):
-                    ore = ops.pop()
-                    if(self.regexp[ore] == '|'):
-                        lp = ops.pop()
-                        self.graph.add_edge(lp, ore + 1, transition=self.regexp[i])
-                        self.graph.add_edge(ore, i, transition=self.regexp[i])
-
-                    elif(self.regexp[ore] == '('):
-                        lp = ore
-                    else:
-                        assert False
-
-                # If there is a kleene star, the code maps back to itself
-                if(i < (self.m-1) and self.regexp[i+1] == '*'):
-                    self.graph.add_edge(lp, i+1, transition=self.regexp[i])
-                    self.graph.add_edge(i+1, lp, transition="empty")
-
-                if(self.regexp[i] == '(' or self.regexp[i] == '|' or self.regexp[i] == '*'):
-                    self.graph.add_edge(i, i+1, transition="empty")
-                else:
-                    self.graph.add_edge(i, i+1, transition=self.regexp[i])
-
-
 
 ##################################################################################
 # readConfig(): read the ini config FILE
@@ -152,9 +131,8 @@ def checkRegex(regex):
 ##################################################################################
 # learnAlphabet(): learn the alphabet of the input file
 ##################################################################################
-def learnAlphabet(inputFile):
+def learnAlphabet(inputContents):
     alphabet = set()
-    inputContents = inputFile.read()
     for character in inputContents:
         if (ord(character) > 47) and (ord(character) < 123):
             alphabet.add(character)
@@ -187,7 +165,8 @@ def main():
     config = readConfig("config/config.ini")
     args = parseArguments()
     if(checkRegex(args.regex)):
-        alphabet = learnAlphabet(args.inputFile)
+        inputContents = args.inputFile.read()
+        alphabet = learnAlphabet(inputContents)
         regexAlpha = learnRegex(args.regex)
         if(config.get('DEBUG', 'printAlpha') == "True"):
              print(alphabet)
@@ -203,14 +182,11 @@ def main():
             else:
                 nfa = NFA(args.regex)
                 print(nfa.graph.edges(data = True))
-                inputContents = args.inputFile.read()
-                args.inputFile.close()
                 recog = nfa.recognizes(inputContents)
                 if(recog == []):
                     print("No Matches")
                 else:
-                    for i in range(0, len(recog)):
-                        print recog + " "
+                    print(recog)
                 # dfa = DFA(nfa)
                 # print(dfa.dfa.edges())
         else:
